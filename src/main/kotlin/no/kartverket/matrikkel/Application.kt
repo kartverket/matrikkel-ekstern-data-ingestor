ebfb269d8eac4db1f30d4f736a5011b39432d15c
Stian Alexander Solli <stian.alexander.solli@kartverket.no>
2026-09-21 15:02:41 +0200

    TH-3220 startConsumer tar inn kafkaConfig og funksjon for håndtering av record

--- src/main/kotlin/no/kartverket/matrikkel/Application.kt
+++ src/main/kotlin/no/kartverket/matrikkel/Application.kt
@@ -2,6 +2,7 @@ package no.kartverket.matrikkel
 
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.Url
 import io.ktor.server.application.install
 import io.ktor.server.cio.CIO
 import io.ktor.server.plugins.callid.CallId
@@ -13,7 +14,12 @@ import io.ktor.server.response.respond
 import no.kartverket.heimdall.common.ktor.plugins.Metrics
 import no.kartverket.heimdall.common.ktor.plugins.selftest.Selftest
 import no.kartverket.heimdall.common.ktor.utils.KtorServer
+import no.kartverket.matrikkel.kafkaclient.InitialOffsetPolicy
+import no.kartverket.matrikkel.kafkaclient.MessageConsumer
+import no.kartverket.matrikkel.kafkaclient.StringSerde
 import org.slf4j.LoggerFactory
+import java.util.UUID
+import kotlin.time.Duration.Companion.seconds
 import kotlin.uuid.Uuid
 
 val applog = LoggerFactory.getLogger("matrikkel-ekstern-data-ingestor")
@@ -46,7 +52,23 @@ fun runApplication(disableSecurity: Boolean = false) {
             this.version = config.version
         }
         configureRouting()
-        startConsumer(config)
+
+        val dummyKafkaConfig = MessageConsumer.Config(
+            server = Url(config.kafkaUrl ?: ""),
+            topic = "my-topic",
+            keySerializer = StringSerde,
+            valueSerializer = StringSerde,
+            correlationIdProvider = { UUID.randomUUID().toString() },
+            maxRetries = 3,
+            consumerGroup = "my-consumer-group",
+            instanceId = "my-instance-1",
+            timeout = 10.seconds,
+            maxRecords = 100,
+            initialOffsetPolicy = InitialOffsetPolicy.LATEST,
+        )
+        startConsumer(dummyKafkaConfig) { record ->
+            applog.info("Polled ${record.key} - ${record.value}")
+        }
 
     }.start(wait = true)
 }
--- src/main/kotlin/no/kartverket/matrikkel/ConsumerService.kt
+++ src/main/kotlin/no/kartverket/matrikkel/ConsumerService.kt
@@ -1,13 +1,9 @@
 package no.kartverket.matrikkel
 
-import io.ktor.http.*
 import io.ktor.server.application.*
 import kotlinx.coroutines.*
 import no.kartverket.matrikkel.kafkaclient.ConsumerRecord
-import no.kartverket.matrikkel.kafkaclient.InitialOffsetPolicy
 import no.kartverket.matrikkel.kafkaclient.MessageConsumer
-import no.kartverket.matrikkel.kafkaclient.StringSerde
-import java.util.*
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
@@ -36,26 +32,14 @@ suspend fun <TKey, TValue> MessageConsumer<TKey, TValue>.consume(
     }
 }
 
-fun Application.startConsumer(config: Configuration) {
-    val kafkaConfig = MessageConsumer.Config(
-        server = Url(config.kafkaUrl ?: ""),
-        topic = "my-topic",
-        keySerializer = StringSerde,
-        valueSerializer = StringSerde,
-        correlationIdProvider = { UUID.randomUUID().toString() },
-        maxRetries = 3,
-        consumerGroup = "my-consumer-group",
-        instanceId = "my-instance-1",
-        timeout = 10.seconds,
-        maxRecords = 100,
-        initialOffsetPolicy = InitialOffsetPolicy.LATEST,
-    )
+fun <TKey, TValue> Application.startConsumer(
+    config: MessageConsumer.Config<TKey, TValue>,
+    onRecord: suspend (ConsumerRecord<TKey, TValue>) -> Unit
+) {
 
-    val consumer: MessageConsumer<String, String> = MessageConsumer.Impl(kafkaConfig)
+    val consumer: MessageConsumer<TKey, TValue> = MessageConsumer.Impl(config)
     val job = launch(Dispatchers.IO) {
-        consumer.consume(10.seconds) { record ->
-            applog.info("Polled ${record.key} - ${record.value}")
-        }
+        consumer.consume(10.seconds, onRecord)
     }
 
     monitor.subscribe(ApplicationStopping) {
