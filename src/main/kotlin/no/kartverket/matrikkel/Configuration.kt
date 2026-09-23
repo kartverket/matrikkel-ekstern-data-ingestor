91b55f6241d812333ffa253a2102d4dd29269c40
Nicklas Utgaard <nutgaard@gmail.com>
2026-09-23 08:41:45 +0200

    TH-3231: konfigurasjon av tokenklient og kafka-oppsett

--- src/main/kotlin/no/kartverket/matrikkel/Configuration.kt
+++ src/main/kotlin/no/kartverket/matrikkel/Configuration.kt
@@ -1,8 +1,11 @@
 package no.kartverket.matrikkel
 
+import io.ktor.http.Url
 import no.kartverket.heimdall.common.kotlin.EnvUtils.getConfig
+import no.kartverket.heimdall.common.tokenclient.client.DownstreamApi
 
 data class Configuration(
     val version: String = getConfig("VERSION"),
-    val kafkaUrl: String = getConfig("KAFKA_URL"),
+    val kafkaBrokerUrl: Url = Url(getConfig("KAFKA_BROKER_URL")),
+    val kafkaBrokerScope: DownstreamApi = DownstreamApi.parse(getConfig("KAFKA_BROKER_SCOPE")),
 )
